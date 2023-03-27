7bcf3c14fc18eeef99ec0f11221a0586ba30bba8
NAYEON KIM <113038790+NaYeonKim1014@users.noreply.github.com>
2023-03-27 10:37:35 +0900

    Update python-maze solving.py

--- python-maze solving.py
+++ python-maze solving.py
@@ -1,24 +1,24 @@
 def find_maze_path(maze):
-    stack = [(mazestart, [mazestart])]  # 시작점 추가, 경로 리스트 추가
-    xn = len(maze) #행개수
-    yn = len(maze[1]) #열개수
-    visited = set() #방문기록저장용기
+    stack = [(mazestart, [mazestart])]  # add starting point, add route list
+    xn = len(maze) # Number of rows
+    yn = len(maze[1]) # Number of columns
+    visited = set() # Save visit history set
 
     
     while stack:
-        (x, y), path = stack.pop()  # 스택에서 위치와 경로 추출
+        (x, y), path = stack.pop()  # Extracting Locations and Paths from stack
         
-        # 방문한 위치인 경우 continue
+        # Continue if where visited
         if (x, y) in visited:
             continue
-        visited.add((x, y)) #방문기록저장
+        visited.add((x, y)) # Save Visit History
 
-        if x == mazeend[0] and y == mazeend[1]:  # 도착점인 경우
-            global mazepath #경로 리스트를 전역 변수로 지정
+        if x == mazeend[0] and y == mazeend[1]:  # if it's the arrival point
+            global mazepath # Set route list as global variable
             mazepath = path
             return True
 
-        # 갈 수 있는 위치 스택에 추가
+        # Add to stack where can go
         if x+1 < xn and maze[x+1][y] == 1:
             stack.append((((x+1, y)), path+[(x+1, y)]))
         if x-1 >= 0 and maze[x-1][y] == 1:
@@ -28,7 +28,7 @@ def find_maze_path(maze):
         if y-1 >= 0 and maze[x][y-1] == 1:
             stack.append((((x, y-1)), path+[(x, y-1)]))
 
-    return False  # 경로가 없는 경우
+    return False  # If there is no path
 
 mazestart = (1,0)
 mazeend = [3,5]
@@ -39,7 +39,7 @@ maze = [[0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0]]
 
 if find_maze_path(maze):
-    print("미로를 찾았습니다.")
-    print("경로 :", mazepath)
+    print("I found the maze.")
+    print("Path :", mazepath)
 else:
-    print("미로를 찾지 못했습니다.")
+    print("I couldn't found the maze.")
