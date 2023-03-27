8bdfae0ee93c3d6c38248c568653c56a7d3d8540
NAYEON KIM <113038790+NaYeonKim1014@users.noreply.github.com>
2023-03-27 10:43:22 +0900

    Update python-postfix expressions.py

--- python-postfix expressions.py
+++ python-postfix expressions.py
@@ -1,12 +1,12 @@
 def evaluate_postfix(lst):
-    op_check = ['+','/','*','-']    # 연산자 체크를 위해 미리 생성
-    stack =[]   # 피연산자 바로 추가할 리스트 생성
+    op_check = ['+','/','*','-']    # Pre-generated for operator check
+    stack =[]   # Create a list to add operands 
     a1=0   # stack[-1]을 위한 변수 생성
     a2=0  # stack[-2]을 위한 변수 생성
-    for l in range(len(lst)):   # 후위표기법으로 저장되 있는 리스트의 수만큼 반복   
-        if lst[l].isdigit():    # 만약 피연산자이면 바로 stack에 추가
+    for l in range(len(lst)):   # Repeat as many lists as are stored as postfix expressions
+        if lst[l].isdigit():    # If it is an operand, add it to the stack immediately
             stack.append(int(lst[l]))
-        elif lst[l] == '+': # + 이면 stack에서 2개 피연산자를 pop하여 게산해준뒤 다시 stack에 추가
+        elif lst[l] == '+': # If it's '+', pop two operands from the stack and add them back to the stack
             a1 = stack.pop()
             a2 = stack.pop()
             stack.append(a1 + a2)
@@ -24,6 +24,6 @@ def evaluate_postfix(lst):
             stack.append(a1 / a2)
     return stack[0]
 
-lst = input("후위 표기식을 입력하세요 :")
+lst = input("Input postfix expression :")
 result = evaluate_postfix(lst)
-print("결과는",result)
+print("Result is",result)
